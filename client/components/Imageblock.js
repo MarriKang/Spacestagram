8cc4577fe5bf8cda354717ac7dbc8e83ff799ac1
Marri Kang <kangmarri@gmail.com>
2021-09-19 13:51:37 -0400

    Initial commit for webpack, babel, and React components

---
+++ client/components/Imageblock.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default Imageblock = () => {
+    return (
+        <div>
+            <h3>Title</h3>
+            <h4>September 19, 2021</h4>
+        </div>
+    )
+}
---
+++ client/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Starstagram</h1>
+                <h2>Brought to you by NASA's image API</h2>
+            </div>
+        )
+    }
+}
