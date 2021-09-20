f60fb183ab3b9fa37ac8134ac1e443f423e8503e
Marri Kang <kangmarri@gmail.com>
2021-09-20 11:32:05 -0400

    Connect API data to component

--- client/components/app.js
+++ client/components/app.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            apiData: {}
+        }
+    }
+
+    async componentDidMount() {
+        const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=yCCnB4GtOJf4czYyEE5uVPoHpQYhpIzdSYhrjcDq');
+        const data = await res.json();
+        this.setState({ apiData: data });
+    }
+
     render() {
         return (
             <div>
