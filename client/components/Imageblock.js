edcd47a838c7ff9cc1d422dcf3000ebafee6b3bb
Marri Kang <kangmarri@gmail.com>
2021-09-20 20:49:48 -0400

    refactor API connection and finish component HTML

--- client/components/Imageblock.js
+++ client/components/Imageblock.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default Imageblock = () => {
-    return (
-        <div>
-            <h3>Title</h3>
-            <h4>September 19, 2021</h4>
-        </div>
-    )
+export default class Imageblock extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            apiData: {},
+            liked: false
+        }
+    }
+
+    toggleLike = (state) => {
+        this.setState({
+            liked: !this.state.liked
+        })
+    }
+
+    //makes a call to the NASA api and gets the picture of the day object
+    async componentDidMount() {
+        const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=yCCnB4GtOJf4czYyEE5uVPoHpQYhpIzdSYhrjcDq');
+        const data = await res.json();
+        this.setState({ apiData: data });
+    }
+
+
+    render() {
+        const likedStatus = this.state.liked ? "Liked" : "Like";
+        const likeButtonClass = this.state.liked ? "liked" : "unliked";
+        return (
+            <div className = "image-block">
+                <img src={this.state.apiData.url} className="image"/>
+                <h3 className="title">{this.state.apiData.title}</h3>
+                <h4 className="subtitle">{this.state.apiData.date}</h4>
+                <button className={likeButtonClass} onClick={this.toggleLike}>{likedStatus}</button>
+            </div>
+        )
+    }
 }
--- client/components/app.js
+++ client/components/app.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
+import Imageblock from './Imageblock';
 
+//arbitrary array to display 6 images
+const arrayForImages = [1,2,3,4,5,6];
 
 export default class App extends Component {
     constructor() {
         super();
-
-        this.state = {
-            apiData: {}
-        }
-    }
-
-    async componentDidMount() {
-        const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=yCCnB4GtOJf4czYyEE5uVPoHpQYhpIzdSYhrjcDq');
-        const data = await res.json();
-        this.setState({ apiData: data });
     }
 
     render() {
         return (
             <div>
-                <h1>Starstagram</h1>
-                <h2>Brought to you by NASA's image API</h2>
+                <header>
+                    <h1>Starstagram</h1>
+                    <h2>Brought to you by NASA's image API</h2>
+                </header>
+                {arrayForImages.map((x, index) => {
+                    return <Imageblock key={index}/>
+                })}
             </div>
         )
     }
