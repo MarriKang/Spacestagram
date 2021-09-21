30e4d406df205f764d9a77c2922818278173c552
Marri Kang <kangmarri@gmail.com>
2021-09-21 17:10:01 -0400

    complete CSS and add mobile formatting

--- client/components/Imageblock.js
+++ client/components/Imageblock.js
@@ -25,13 +25,15 @@ export default class Imageblock extends Component {
 
 
     render() {
-        const likedStatus = this.state.liked ? "Liked" : "Like";
+        const likedStatus = this.state.liked ? "LIKED" : "LIKE";
         const likeButtonClass = this.state.liked ? "liked" : "unliked";
         return (
             <div className = "image-block">
                 <img src={this.state.apiData.url} className="image"/>
-                <h3 className="title">{this.state.apiData.title}</h3>
-                <h4 className="subtitle">{this.state.apiData.date}</h4>
+                <div className="block-text">
+                    <h3 className="title">{this.state.apiData.title}</h3>
+                    <h4 className="subtitle">{this.state.apiData.date}</h4>
+                </div>
                 <button className={likeButtonClass} onClick={this.toggleLike}>{likedStatus}</button>
             </div>
         )
